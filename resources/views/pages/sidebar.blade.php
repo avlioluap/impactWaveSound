73cf53644113adaba288b37cad090ff7f3f25748
paulo silva <adsilva.paulo@gmail.com>
2017-02-22 22:30:26 +0000

    more css

--- resources/views/pages/sidebar.blade.php
+++ resources/views/pages/sidebar.blade.php
@@ -8,33 +8,39 @@
     	<ul>
             <li {{ (Request::is('search') ? 'class=active' : '') }} >
             	<a href="{{ url('/search') }}" class="sideBarLink">
+            		<i class="fa fa-search" aria-hidden="true"></i>
             		@lang('sidebar.search')
             	</a>
             </li>
             <li {{ (Request::is('browse') ? 'class=active' : '') }} >
             	<a href="{{ url('/browse') }}" class="sideBarLink">
+            		<i class="fa fa-list-ul" aria-hidden="true"></i>
             		@lang('sidebar.browse')
             	</a>
 
             </li>
             <li {{ (Request::is('playlist') ? 'class=active' : '') }} >
             	<a href="{{ url('/playlist') }}" class="sideBarLink">
+	            	<i class="fa fa-youtube-play" aria-hidden="true"></i>
             		@lang('sidebar.playlist')
             	</a>
 
-            </li>            
+            </li>
             <li {{ (Request::is('music') ? 'class=active' : '') }} >
             	<a href="{{ url('/music') }}" class="sideBarLink">
+	            	<i class="fa fa-headphones" aria-hidden="true"></i>
             		@lang('sidebar.music')
             	</a>
             </li>
             <li {{ (Request::is('account') ? 'class=active' : '') }} >
             	<a href="{{ url('/account') }}" class="sideBarLink">
+            		<i class="fa fa-user" aria-hidden="true"></i>
             		{{ Auth::getUser()->name }}
             	</a>
             </li>
             <li {{ (Request::is('settings') ? 'class=active' : '') }}>
             	<a href="{{ url('/settings') }}" class="sideBarLink">
+            		<i class="fa fa-cog" aria-hidden="true"></i>
             		@lang('sidebar.settings')
             	</a>
             </li>
